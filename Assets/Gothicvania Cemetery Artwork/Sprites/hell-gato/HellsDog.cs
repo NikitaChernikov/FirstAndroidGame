70054be59b7147952eadf1fee29343582ff8b687
NikitaChernikov <nikitachernikov20006@gmail.com>
2021-01-16 13:53:39 +0600

    Hell's Dog enemy added

---
+++ Assets/Gothicvania Cemetery Artwork/Sprites/hell-gato/HellsDog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellsDog : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Animator anim;
+    Rigidbody2D rb;
+    public int distance;
+    float maxDistance;
+    float minDistance;
+    int speed = 3;
+
+    private void Start()
+    {
+        
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        maxDistance = transform.position.x + distance;
+        minDistance = transform.position.x - distance;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(transform.right * speed * Time.deltaTime);
+        if (transform.position.x > maxDistance)
+        {
+            sr.flipX = false;
+            speed = -speed;
+        }
+        else if (transform.position.x < minDistance)
+        {
+            sr.flipX = true;
+            speed = -speed;
+        }
+    }
+
+}
